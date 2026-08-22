c33402699c3d3dcf9f347370c6cec116354d8d08
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2026-08-22 02:05:29 -0400

    Revert "fix!: ctx.Decode/Encode -> router.Decode/Encode (funciones libres genericas tras bump de router a v0.1.26)"
    This reverts commit 6dd6695aa61d4e1db2eef9d29745136992170868.

--- tests/harvest_test.go
+++ tests/harvest_test.go
@@ -27,11 +27,11 @@ func (fakeModule) ModelName() string { return "fake" }
 func (fakeModule) MountOps(r router.OpRegistry) {
 	r.Op("do_thing", func(ctx router.Context) {
 		var in fakeArgs
-		if err := router.Decode(ctx, &in); err != nil {
+		if err := ctx.Decode(&in); err != nil {
 			ctx.WriteStatus(500)
 			return
 		}
-		_ = router.Encode(ctx, &fakeArgs{Value: "echo:" + in.Value})
+		_ = ctx.Encode(&fakeArgs{Value: "echo:" + in.Value})
 	}).Requires("fake_resource", model.Read).Accepts(&fakeArgs{})
 }
 
