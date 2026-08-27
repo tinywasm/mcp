ae9ef9f2eed6052008007b935ea850f811abea33
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2026-08-27 08:23:03 -0400

    fix: implement router.Context Param for mcp opContext
    4 files changed, 5 insertions(+), 4 deletions(-)

--- harvest.go
+++ harvest.go
@@ -87,6 +87,7 @@ func (c *opContext) WriteStatus(code int)                     { c.status = code 
 func (c *opContext) Write(b []byte) (int, error)              { c.body = append([]byte{}, b...); return len(b), nil }
 func (c *opContext) SetValue(string, string)                  {}
 func (c *opContext) Value(string) string                      { return "" }
+func (c *opContext) Param(string) string                      { return "" }
 func (c *opContext) SetCookie(router.Cookie)                  {}
 func (c *opContext) Cookie(string) (router.Cookie, bool)      { return router.Cookie{}, false }
 func (c *opContext) SetUserID(id string)                      { c.userID = id }
